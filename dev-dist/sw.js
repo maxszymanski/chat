7e9656eb11f501c0c45c0722f2ec1e649f3df63c
Max Szymański <maksymilianszymanski2@gmail.com>
2024-10-18 14:52:42 +0200

    save

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -13,66 +13,64 @@
 
 // If the loader is already loaded, just stop.
 if (!self.define) {
-    let registry = {}
+  let registry = {};
 
-    // Used for `eval` and `importScripts` where we can't get script URL by other means.
-    // In both cases, it's safe to use a global var because those functions are synchronous.
-    let nextDefineUri
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
 
-    const singleRequire = (uri, parentUri) => {
-        uri = new URL(uri + '.js', parentUri).href
-        return (
-            registry[uri] ||
-            new Promise((resolve) => {
-                if ('document' in self) {
-                    const script = document.createElement('script')
-                    script.src = uri
-                    script.onload = resolve
-                    document.head.appendChild(script)
-                } else {
-                    nextDefineUri = uri
-                    importScripts(uri)
-                    resolve()
-                }
-            }).then(() => {
-                let promise = registry[uri]
-                if (!promise) {
-                    throw new Error(`Module ${uri} didn’t register its module`)
-                }
-                return promise
-            })
-        )
-    }
-
-    self.define = (depsNames, factory) => {
-        const uri =
-            nextDefineUri ||
-            ('document' in self ? document.currentScript.src : '') ||
-            location.href
-        if (registry[uri]) {
-            // Module is already loading or loaded.
-            return
-        }
-        let exports = {}
-        const require = (depUri) => singleRequire(depUri, uri)
-        const specialDeps = {
-            module: { uri },
-            exports,
-            require,
-        }
-        registry[uri] = Promise.all(
-            depsNames.map((depName) => specialDeps[depName] || require(depName))
-        ).then((deps) => {
-            factory(...deps)
-            return exports
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
         })
-    }
-}
-define(['./workbox-b5f7729d'], function (workbox) {
-    'use strict'
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
 
-    self.skipWaiting()
-    workbox.clientsClaim()
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
+
+  self.skipWaiting();
+  workbox.clientsClaim();
 
   /**
    * The precacheAndRoute() method efficiently caches and responds to
@@ -84,7 +82,7 @@ define(['./workbox-b5f7729d'], function (workbox) {
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bf5gp5iivg"
+    "revision": "0.48em9l3dh2o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
