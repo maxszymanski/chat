4a742a4e3ac54f1d1091e6106805e0d203363ce5
Max Szymański <maksymilianszymanski2@gmail.com>
2024-10-22 13:58:33 +0200

    add loader if messages are loading

--- src/chat/Camera.tsx
+++ src/chat/Camera.tsx
@@ -1,71 +1,11 @@
-import { useRef, useState } from 'react'
 import FileButton from '../components/FileButton'
 import { CameraIcon } from '@heroicons/react/16/solid'
 
 function Camera() {
-    const videoRef = useRef(null)
-    const canvasRef = useRef(null)
-    const [isCameraOpen, setIsCameraOpen] = useState(false)
-
-    const handleOpenCamera = async () => {
-        setIsCameraOpen(true)
-        try {
-            const stream = await navigator.mediaDevices.getUserMedia({
-                video: true,
-            })
-            if (videoRef.current) {
-                videoRef.current.srcObject = stream
-            }
-        } catch (err) {
-            console.error('Error accessing camera: ', err)
-        }
-    }
-
-    const handleTakePhoto = () => {
-        if (canvasRef.current && videoRef.current) {
-            const context = canvasRef.current.getContext('2d')
-            context.drawImage(
-                videoRef.current,
-                0,
-                0,
-                canvasRef.current.width,
-                canvasRef.current.height
-            )
-            const imageData = canvasRef.current.toDataURL('image/png')
-            console.log('Captured image: ', imageData) // Możesz zapisać to zdjęcie lub wysłać je do backendu
-        }
-    }
-
     return (
-        <div className="">
-            {isCameraOpen ? (
-                <>
-                    <video
-                        ref={videoRef}
-                        autoPlay
-                        className="border-2 border-gray-500"
-                        width="300"
-                        height="300"
-                    />
-                    <canvas
-                        ref={canvasRef}
-                        width="300"
-                        height="300"
-                        style={{ display: 'none' }}
-                    />
-                    <button
-                        onClick={handleTakePhoto}
-                        className="bg-blue-500 text-white py-2 px-4 rounded mt-4"
-                    >
-                        Take Photo
-                    </button>
-                </>
-            ) : (
-                <FileButton onClick={handleOpenCamera}>
-                    <CameraIcon className="h-7 w-7" />
-                </FileButton>
-            )}
-        </div>
+        <FileButton onClick={() => {}}>
+            <CameraIcon className="h-7 w-7" />
+        </FileButton>
     )
 }
 
--- src/chat/ChatMain.tsx
+++ src/chat/ChatMain.tsx
@@ -1,9 +1,12 @@
 import EmptyChat from '../components/EmptyChat'
+import Loader from '../components/Loader'
 import { useMessages } from '../hooks/useMessages'
 import MessagesList from './MessagesList'
 
 function ChatMain() {
-    const { messages } = useMessages()
+    const { messages, isLoading } = useMessages()
+
+    if (isLoading) return <Loader />
 
     return (
         <main className="flex-1 overflow-y-auto px-6 py-6 text-lg">
